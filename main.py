f8a74a7fb54d39356e8ef8cb0cebaa6ff30456a2
muhammed bolat <mmbolatt@gmail.com>
2023-11-11 22:45:15 +0300

    create Procfile,addEV

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+#app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 bootstrap = Bootstrap5(app)
 
@@ -39,7 +41,8 @@ gravatar = Gravatar(app,
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -255,4 +258,4 @@ def contact():
 
 
 if __name__ == "__main__":
-	app.run(debug=True,  port=5001)
+	app.run(debug=False,  port=5001)
